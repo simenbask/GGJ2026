889ed75c9b68e578ce0cab67f258567762adfd7b
Simen <simskje@gmail.com>
2026-02-01 18:13:42 +0100

    fix: make bitdisplay disappear with item

--- Assets/Scripts/Engine/Item.cs
+++ Assets/Scripts/Engine/Item.cs
@@ -17,12 +17,18 @@ namespace simenbask.GGJBit.Engine
 
         [SerializeField]
         private DoorItemClick _doorClick;
+
         private void Start()
         {
             _bitDisplay = BitDisplayManager.BitDisplayPool.Get();
             _bitDisplay.Initialize(BitItem);
             BitItem.Initialize();
             BitItem.OnBecomeDoor += BecomeDoor;
+        }
+
+        private void OnDisable()
+        {
+            BitDisplayManager.BitDisplayPool.Release(_bitDisplay);
         }
 
         private void BecomeDoor(bool door)
